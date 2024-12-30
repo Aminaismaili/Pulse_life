ca7dce2573a6f38104ebfa16ac9893d509a8f0ea
Aminaismaili <ami.ismaili@edu.umi.ac.ma>
2024-12-30 12:11:14 +0100

    project

--- docs/conf.py
+++ docs/conf.py
@@ -24,4 +24,9 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
+
+
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
